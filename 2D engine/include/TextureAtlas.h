aafef75b65fad2d410e0f3ff1f1890a349df9d02
Nikolay <niki.donev20@gmail.com>
2026-09-17 16:55:02 +0300

    Refactor getTexCoords method signatures in TextureAtlas

--- 2D engine/include/TextureAtlas.h
+++ 2D engine/include/TextureAtlas.h
@@ -5,7 +5,7 @@
 
 class TextureAtlas {
 public:
-	TextureAtlas::TextureAtlas(const Ref<Texture>& textureRef, uint32_t verticalCount, uint32_t horizontalCount)
+	TextureAtlas(const Ref<Texture>& textureRef, uint32_t verticalCount, uint32_t horizontalCount)
 		: texture(textureRef), horizontalCount(horizontalCount), verticalCount(verticalCount) {
 		uint32_t height = texture->GetHeight(), width = texture->GetWidth();
 		m_SpriteWidth = width / horizontalCount;
@@ -13,14 +13,14 @@ public:
 		std::cout << width << " " << m_SpriteWidth << " " << height << " " << m_SpriteHeight << std::endl;
 	}
 
-	glm::vec4 TextureAtlas::getTexCoords(glm::ivec2 selected,float correction = 0.05f) const {
+	glm::vec4 getTexCoords(glm::ivec2 selected,float correction = 0.05f) const {
 		int& x = selected.x, y = verticalCount - selected.y - 1;
 		float height = (float)texture->GetHeight(), width = (float)texture->GetWidth();
 		float top = (y + 1) * m_SpriteHeight - correction, bottom = y * m_SpriteHeight + correction;
 		float left = x * m_SpriteWidth + correction, right = (x + 1) * m_SpriteWidth - correction;
 		return { left / width, bottom / height, right / width, top / height };
 	}
-	glm::vec4 TextureAtlas::getTexCoords(int atlasIndex, float correction = 0.05f) const {
+	glm::vec4 getTexCoords(int atlasIndex, float correction = 0.05f) const {
 		int x = atlasIndex % verticalCount, y = atlasIndex / verticalCount;
 		return getTexCoords({ x,y }, correction);
 	}
